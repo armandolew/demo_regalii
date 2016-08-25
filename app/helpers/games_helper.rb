343daa0a3326fedc4b43fb49ebf317b7833cf3aa
armandolew <alew16@hotmail.com>
2016-08-24 19:55:15 -0500

    fix upset functionality

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -24,21 +24,37 @@ module GamesHelper
   end 
 
   def award_points(game)
+    p game.inspect
     if game.player_1_score.to_i > game.player_2_score.to_i
       ranking = Ranking.find_by(:user_id => game.player_1_id)
-      if ranking
-        points = ranking.points.to_i + 3
+      if !ranking.nil?
+        points = ranking.points.to_i + 3 + upset(game.player_1_id, game.player_2_id)
         ranking.update(:points => points)
       end      
       return game.player_1_id
     else
       ranking = Ranking.find_by(:user_id => game.player_2_id)
-      if ranking
-        points = ranking.points.to_i + 3
+      if !ranking.nil?
+        points = ranking.points.to_i + 3 + upset(game.player_2_id, game.player_1_id)
         ranking.update(:points => points)
       end      
       return game.player_2_id
     end
+  end
+
+  def upset(player_1_id, player_2_id)
+    ranking_player_1 = Ranking.find_by(:user_id => player_1_id)
+    ranking_player_2 = Ranking.find_by(:user_id => player_2_id)
+  
+    upset = 0
+   
+
+    if !ranking_player_1.nil? and !ranking_player_2.nil?
+      if ranking_player_1.points < ranking_player_2.points
+        upset = 2
+      end
+    end
+    return upset
   end
 
   def games_played(user_id)
